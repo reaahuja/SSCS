232f95f689cde33ba39d07001a279cbfeb39b36e
Rea Ahuja <98184500+reaahuja@users.noreply.github.com>
2023-06-20 19:13:35 -0400

    Update slider.ino

--- slider.ino
+++ slider.ino
@@ -1,21 +1,21 @@
-const int potPin = A0;    // Analog input pin for the potentiometer
-const int buzzerPin = 9;  // Digital output pin for the buzzer
+const int potPin = A0;    
+const int buzzerPin = 9;  
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);  // Set the buzzer pin as an output
-  Serial.begin(9600);          // Initialize serial communication
+  Serial.begin(9600);          
 }
 
 void loop() {
   int potValue = analogRead(potPin);        // Read the analog value from the potentiometer
   int frequency = map(potValue, 0, 1023, 100, 2000);  // Map the potentiometer value to a frequency range
   
-  tone(buzzerPin, frequency);  // Play the tone on the buzzer
+  tone(buzzerPin, frequency);  // Play tone on buzzer
   
   Serial.print("Potentiometer Value: ");
   Serial.print(potValue);
   Serial.print(", Frequency: ");
   Serial.println(frequency);
   
-  delay(100);  // Small delay between readings
+  delay(100);  // Small delay
 }
