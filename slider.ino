606d4fbe521d2ef4bf7aa103f5e9499bff801a57
Rea Ahuja <98184500+reaahuja@users.noreply.github.com>
2023-06-21 09:50:01 -0400

    Update slider.ino

--- slider.ino
+++ slider.ino
@@ -1,19 +1,19 @@
-const int potPin = A0;    // Analog input pin for the potentiometer
-const int buzzerPin = 8;  // Digital output pin for the buzzer
+const int potPin = A0;    // Analog for the potentiometer
+const int buzzerPin = 8;  // Digital for the buzzer
 
 void setup() {
-  pinMode(buzzerPin, OUTPUT);  // Set the buzzer pin as an output
-  Serial.begin(9600);          // Initialize serial communication
+  pinMode(buzzerPin, OUTPUT);  
+  Serial.begin(9600);         
 }
 
 void loop() {
   int potValue = analogRead(potPin);        // Read the analog value from the potentiometer
-  int volume = map(potValue, 0, 1023, 0, 255);  // Map the potentiometer value to a volume range
+  int volume = map(potValue, 0, 1023, 0, 255);  // Map the potentiometer
   
   analogWrite(buzzerPin, volume);  // PMW the buzzer 
   
   Serial.println("Potentiometer Value: ");
   Serial.println(potValue);
   
-  delay(1000);  // Small delay between readings
+  delay(1000);  
 }
