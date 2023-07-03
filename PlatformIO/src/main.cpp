a9c2135e78d759d7abe29c137ff668ae964270af
Rea Ahuja <98184500+reaahuja@users.noreply.github.com>
2023-07-03 16:40:49 -0400

    Update main.cpp

--- PlatformIO/src/main.cpp
+++ PlatformIO/src/main.cpp
@@ -1,17 +1,23 @@
 #include <Synth.h>
 #include <Ultrasonic.h>
+#include <Servo.h>
 
 Synth synth(9);
 Ultrasonic ultrasonic(12, 13);
+Servo servoMotor;
 
 int volumes[3] = { 50, 150, 255 };
 // int frequencies[14] = { 440, 494, 523, 587, 659, 698, 784, 880, 988, 1047, 1175, 1319, 1397, 1568 };
 int frequencies[8] = { 1047, 1175, 1319, 1397, 1568, 1760, 1976, 2093 };
+int photoResistor = A1; 
+int lightReading; 
 
 void setup() {
   Serial.begin(115200);
   synth.tone_type(SQUARE_WAVE);
   pinMode(A0, INPUT);
+  pinMode(photoResistor, INPUT);
+  servoMotor.attach(9);
 }
 
 void loop() {
@@ -33,7 +39,6 @@ void loop() {
   // }
   // synth.stop();
   // delay(1000);
-
   int volRaw = map(analogRead(A0), 0, 1023, 0, 255);
   int fIdxRaw = map(ultrasonic.read(CM), 0, 12, 0, 7);
 
@@ -44,6 +49,16 @@ void loop() {
   Serial.print(freq);
   Serial.print(" Hz ");
   Serial.println(vol);
+
+  lightReading = analogRead(photoResistor);
+  lightReading = map(lightReading, 1023, 0, 1, 0);
+  if (value == 1){
+    servoMotor.write(180);
+    delay(50);
+  }
+  servoMotor.write(0);
+  
+
 
   synth.tone(freq, vol);
   delay(50);
