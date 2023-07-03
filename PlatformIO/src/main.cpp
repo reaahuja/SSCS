a53b3c12d9b96610747e88ff4cdf5f4f5c1d8bf4
Rea Ahuja <98184500+reaahuja@users.noreply.github.com>
2023-07-03 18:04:37 -0400

    Update main.cpp

--- PlatformIO/src/main.cpp
+++ PlatformIO/src/main.cpp
@@ -1,97 +1,33 @@
+#include <Servo.h>
 #include <Synth.h>
 #include <Ultrasonic.h>
-#include <Servo.h>
 
+Servo servoMotor;
 Synth synth(9);
 Ultrasonic ultrasonic(12, 13);
-Servo servoMotor;
 
 int volumes[3] = { 50, 150, 255 };
 // int frequencies[14] = { 440, 494, 523, 587, 659, 698, 784, 880, 988, 1047, 1175, 1319, 1397, 1568 };
 int frequencies[8] = { 1047, 1175, 1319, 1397, 1568, 1760, 1976, 2093 };
 
+
 void setup() {
   Serial.begin(115200);
   synth.tone_type(SQUARE_WAVE);
   pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  servoMotor.attach(9);
+  servoMotor.attach(10);
 }
 
 void loop() {
-  // int freq, vol;
-  // for (int i = 0; i < 3; i++) {
-  //   for (int j = 0; j < 8; j++) {
-  //     vol = volumes[i];
-  //     freq = frequencies[j];
-
-  //     Serial.print(freq);
-  //     Serial.print(" Hz ");
-  //     Serial.println(vol);
-
-  //     synth.tone(freq, vol);
-  //     delay(100);
-  //   }
-  //   synth.stop();
-  //   delay(500);
-  // }
-  // synth.stop();
-  // delay(1000);
-  int volRaw = map(analogRead(A0), 0, 1023, 0, 255);
-  int fIdxRaw = map(ultrasonic.read(CM), 0, 12, 0, 7);
-
-  int vol = constrain(volRaw, 0, 255);
-  int fIdx = constrain(fIdxRaw, 0, 7);
-  int freq = frequencies[fIdx];
-
-  int lightReading = analogRead(A1);
-  //lightReading = map(lightReading, 1023, 0, 10, 0);
-
-  int servo_position = 700;
-  int speed = 6;
-  
-  //while(true){ 
-  if(servo_position < lightReading){
-    servo_position += speed;
-  }
-  if(servo_position > lightReading){
-    servo_position -= speed;
-  }
-  // if(abs(servo_position - target)<speed){
-  //   break;
-  // }
-  //}
-  pwm_out(servo_position);
-  
-
-  synth.tone(freq, vol);
-  delay(50);
-}
-
-void pwm_out(int position) {
-  int pulseDuration = map(position, 0, 180, 1000, 2000); 
-  servoMotor.writeMicroseconds(pulseDuration); 
-}
-
-/*
-#include <Servo.h>
-
-Servo servoMotor;
-
-void setup() {
-  servoMotor.attach(9);
-}
-
-void loop() {
-  int servo_position = 1500; // arbitrary starting values
+  int servo_position = 1500; 
   int target = 500;
-  const int speed = 5;
-  //int speed = analogRead(A1);
+  int speed = analogRead(A1);
 
+  
   while (true) {
     if (servo_position < target) {
       servo_position += speed;
-    }
+    } 
     if (servo_position > target) {
       servo_position -= speed;
     }
@@ -99,12 +35,19 @@ void loop() {
       break;
     }
     pwm_out(servo_position);
-    delay(20);
+
+    int volRaw = map(analogRead(A0), 0, 1023, 0, 255);
+    int fIdxRaw = map(ultrasonic.read(CM), 0, 12, 0, 7);
+
+    int vol = constrain(volRaw, 0, 255);
+    int fIdx = constrain(fIdxRaw, 0, 7);
+    int freq = frequencies[fIdx];
+
+    synth.tone(freq, vol);  
+    delay(50);
   }
 }
 
 void pwm_out(int position) {
   servoMotor.writeMicroseconds(position);
 }
-
-*/
