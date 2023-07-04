1000b7f033f055f33dc8fc6b24dab5cad28961a8
Rea Ahuja <98184500+reaahuja@users.noreply.github.com>
2023-07-03 20:30:35 -0400

    Update main.cpp

--- PlatformIO/src/main.cpp
+++ PlatformIO/src/main.cpp
@@ -10,6 +10,8 @@ int volumes[3] = { 50, 150, 255 };
 // int frequencies[14] = { 440, 494, 523, 587, 659, 698, 784, 880, 988, 1047, 1175, 1319, 1397, 1568 };
 int frequencies[8] = { 1047, 1175, 1319, 1397, 1568, 1760, 1976, 2093 };
 
+void pwn_out(int position);
+
 
 void setup() {
   Serial.begin(115200);
@@ -20,22 +22,23 @@ void setup() {
 }
 
 void loop() {
-  int servo_position = 1500; 
+
+  int servo_position = 1500;
   int target = 500;
   int speed = analogRead(A1);
 
-  
+
   while (true) {
     if (servo_position < target) {
       servo_position += speed;
-    } 
+    }
     if (servo_position > target) {
       servo_position -= speed;
     }
     if (abs(servo_position - target) < speed) {
       break;
     }
-    pwm_out(servo_position);
+    servoMotor.writeMicroseconds(servo_position);
 
     int volRaw = map(analogRead(A0), 0, 1023, 0, 255);
     int fIdxRaw = map(ultrasonic.read(CM), 0, 12, 0, 7);
@@ -44,11 +47,9 @@ void loop() {
     int fIdx = constrain(fIdxRaw, 0, 7);
     int freq = frequencies[fIdx];
 
-    synth.tone(freq, vol);  
+    synth.tone(freq, vol);
     delay(50);
   }
 }
 
-void pwm_out(int position) {
-  servoMotor.writeMicroseconds(position);
-}
+
